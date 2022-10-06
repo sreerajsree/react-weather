d84c50b327e39f2267f0555d5e210a1617e75c87
sreerajsree <sreeandsreeraj@gmail.com>
2022-10-06 20:00:13 +0530

    data

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
     }
   };
 
-  const Temp = (data.main.temp - 32)*5/9;
-  const Fl = (data.main.feels_like - 32)*5/9;
+  const tempData = ((data.main.temp.toFixed() - 32)*5/9);
+  const feelsLike = ((data.main.feels_like.toFixed() - 32)*5/9);
 
   return (
     <div className="app">
@@ -40,7 +40,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{Temp.toFixed()}°C</h1> : null}
+            {data.main ? <h1>{tempData.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -53,7 +53,7 @@ function App() {
           <div className="bottom">
             <div className="feels">
               {data.main ? (
-                <p className="bold">{Fl.toFixed()}°C</p>
+                <p className="bold">{feelsLike.toFixed()}°C</p>
               ) : null}
               <p>Feels Like</p>
             </div>
